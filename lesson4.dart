b402e44fa0b586b7eb01680e3d81fc84a9d9341f
chikaasan <chyngyz.asanhodjoevv@gmail.com>
2021-03-17 06:41:57 +0300

    first task

---
+++ lesson4.dart
@@ -0,0 +1,45 @@
+main() {
+  int a = 1;
+  int b = 2;
+  int c = 3; 
+  int d = 4;
+  int e = 5;
+
+  double x = 3.5;
+  double y = 4.5;
+  double z = 5.5;
+  double l = 6.5;
+  double f = 7.5;
+
+  String name = "Oleg";
+  String color = "Red";
+  String country = "Russion";
+  String car = "BMW";
+  String clothes = "Jacked";
+  
+  bool box1 = false;
+  bool box2 = true;
+  bool box3 = false;
+  bool box4 = true;
+  bool box5 = false;
+
+  List month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  List week = [1, 2, 3, 4, 5, 6, 7];
+  List monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decembem"];
+  List weekName = ["Monday", " Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  List hours = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+  
+  Map student1 = {'name':'Alena', 'age':20,};
+  Map student2 = {'name':'Nick', 'age':21};
+  Map student3 = {'name':'Bob', 'age':22};
+  Map student4 = {'name':'Petr', 'age':33};
+  Map student5 = {'name':'Lola', 'age':20};
+
+  var a1 = "\u{1F466}";
+  var a2 = "\u2665";
+  var a3 = '\u{1f600}';
+  var a4 = "\U+1F469";
+  var a5 = "\U+200D";
+
+  print("$week $month");
+}
